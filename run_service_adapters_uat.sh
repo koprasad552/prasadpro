bc53fa6afc74ebedf8e96faf2f6fa013c40da822
Prasad <37779460+koprasad552@users.noreply.github.com>
2022-01-31 09:24:03 +0530

    Update run_service_adapters_uat.sh

--- run_service_adapters_uat.sh
+++ run_service_adapters_uat.sh
@@ -1,10 +1,10 @@
-cd ../../../GatewayServices
+cd GatewayServices
 find . -type f -name "pom.xml" -print0 | xargs -0 sed -i '' -e 's#<mfpfUrl>http://localhost:9080/mfpadmin%3C/mfpfUrl%3E#%3CmfpfUrl%3Ehttp://10.195.140.175:9080//mfpadmin%3C/mfpfUrl%3E#g'
 
 echo ======================Generate Gateway Lib Jar file======================
-(cd ../GatewayServicesLib;mvn clean:clean)
-(cd ../GatewayServicesLib;mvn clean install)
-(cd ../GatewayServicesLib;mvn package)
+(cd GatewayServicesLib;mvn clean:clean)
+(cd GatewayServicesLib;mvn clean install)
+(cd GatewayServicesLib;mvn package)
 
 echo ====================== Building Gateway Adapters ======================
 mvn clean
